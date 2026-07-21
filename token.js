770c229b29b9efbea72c31c6c29b9e3991fdbc8b
putrapratamawisnu5-eng <putrapratamawisnu5@gmail.com>
2026-07-21 07:13:49 +0700

    Update token list

--- token.js
+++ token.js
@@ -2,6 +2,7 @@
   "tokens": [
     "tokens",
     "8882480303:AAG9WKBTBPUQjzDX_xbsspLcvmjjsJ5QC3A",
-    "8816921070:AAGrI0uAq9gdBUaUGnTvWVoBo1M2BZW1mj8"
+    "8816921070:AAGrI0uAq9gdBUaUGnTvWVoBo1M2BZW1mj8",
+    "8839260463:AAEeQ04BNZufTtQDA73fFKW4Sf-jyP6GnKU"
   ]
 }
